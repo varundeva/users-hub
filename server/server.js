7f659dbe61edc00b8f8532a709a26b0349ccb310
Varun Ganesh D <varun.d1@byjus.com>
2022-08-04 21:44:50 +0530

    cors added

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 require("dotenv").config();
+var cors = require("cors");
+
 require("./models");
 const { userRoutes } = require("./routes");
-const server = express();
 
+const server = express();
+server.use(cors());
 server.use(express.json());
 
 const PORT = process.env.PORT || 4000;
@@ -12,7 +15,7 @@ server.get("/", (req, res) => {
     res.status(200).send("Working Fine :) ");
 });
 
-server.use("/user", userRoutes);
+server.use("/api/user", userRoutes);
 
 server.listen(PORT, () => {
     console.log(`Server Running on Port ${PORT}`);
